d62af912ea095a7ec13095894518298f9b967507
kuda0313 <kuda0313@gmail.com>
2025-08-14 01:35:26 +0800

    fix hold t2

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -62,7 +62,12 @@ lazy_static::lazy_static! {
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
-    pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+    pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = {
+        let mut map = HashMap::new();
+        // 添加一次性密碼長度預設為8
+    	map.insert("temporary-password-length".to_owned(), "8".to_owned());
+        RwLock::new(map)
+    };
     pub static ref OVERWRITE_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
@@ -74,9 +79,9 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("disable-account".to_owned(), "Y".to_owned());
+        //map.insert("disable-account".to_owned(), "Y".to_owned());
         map.insert("hide-network-settings".to_owned(), "Y".to_owned());
-            map.insert("hide-remote-printer-settings".to_owned(), "Y".to_owned());
+        map.insert("hide-remote-printer-settings".to_owned(), "Y".to_owned());
         RwLock::new(map)
     };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
